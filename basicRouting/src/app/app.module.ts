271a8121e9f17f241807aa948049ada3718a5405
muxudiinkeey <57723477+muxudiinkeey@users.noreply.github.com>
2022-03-24 20:33:57 +0100

    ok

--- basicRouting/src/app/app.module.ts
+++ basicRouting/src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AbouteComponent } from './aboute/aboute.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { PathNotFoundComponentComponent } from './path-not-found-component/path-not-found-component.component';
+// samee Routes xarafka hoe kabital xusuuso
+const route: Routes = [
+  {path: '' , component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'aboute', component: AbouteComponent},
+  {path: 'contact', component: ContactComponent},
+  { path: '**', component: PathNotFoundComponentComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AbouteComponent,
     ContactComponent,
-    HomeComponent
+    HomeComponent,
+    PathNotFoundComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route) //route waa in aan ku qoraa halkan
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ basicRouting/src/app/path-not-found-component/path-not-found-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-path-not-found-component',
+  templateUrl: './path-not-found-component.component.html',
+  styleUrls: ['./path-not-found-component.component.css']
+})
+export class PathNotFoundComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
