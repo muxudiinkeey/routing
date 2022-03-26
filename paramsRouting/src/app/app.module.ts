7de573a6b5e84f214f45492a6d46a9c80fa9dec0
muxudiinkeey <57723477+muxudiinkeey@users.noreply.github.com>
2022-03-26 18:08:59 +0100

    ok

---
+++ paramsRouting/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AbouteComponent } from './aboute/aboute.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+// samee Routes xarafka hoe kabital xusuuso
+const route: Routes = [
+  {path: '' , redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'aboute', component: AbouteComponent},
+  {path: 'contact', component: ContactComponent},
+  { path: '**', component: PageNotFoundComponent},
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AbouteComponent,
+    ContactComponent,
+    HomeComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(route) //route waa in aan ku qoraa halkan
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
