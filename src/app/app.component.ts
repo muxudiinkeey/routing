bf836a7d30c0221e173cfab795479663e9f3819d
Mohiaden Mohamoud <57723477+muxudiinkeey@users.noreply.github.com>
2025-01-19 15:43:56 +0100

    deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';import { UserComponent } from './user/user.component';
+import {  RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AbouteComponent } from './aboute/aboute.component';
 import { ContactComponent } from './contact/contact.component';
-import { UserComponent } from './user/user.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { EditUsetComponent } from './edit-uset/edit-uset.component';
+
+
+
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'aboute', component: AbouteComponent},
     { path: 'contact', component: ContactComponent},
-    { path: 'user', component: UserComponent},
-    { path: 'userList', component: UserListComponent },
-    { path: 'EditUser', component: EditUsetComponent }
 
 
 
