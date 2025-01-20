ae416ccb937c0af1882c74abd448483697cb84f0
Mohiaden Mohamoud <57723477+muxudiinkeey@users.noreply.github.com>
2025-01-20 11:55:44 +0100

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
