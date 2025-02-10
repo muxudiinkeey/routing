765514dcbff1db67982e2c21ec687cf803bd0408
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-10 11:51:55 +0100

    deleted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { AbouteComponent } from './aboute/aboute.component';
 import { ContactComponent } from './contact/contact.component';
 import { UserComponent } from './user/user.component';
-import { UserDetComponent } from './user-det/user-det.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'aboute', component: AbouteComponent},
     { path: 'contact', component: ContactComponent},
     { path: 'user', component: UserComponent},
-    { path: 'userDet/:id', component: UserDetComponent},
     
     
     
