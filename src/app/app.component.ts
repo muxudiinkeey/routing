656fbc4529e48ed3ba71084dda964fa6a0156232
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-11 11:25:00 +0100

    kuli waa ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';import { UserComponent } from './user/user.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    id: string
+    name: string
+    description: string
+    features: string
+    price: string
+    keywords: string
+    url: string
+    category: string
+    subcategory: string
+  }
+  
