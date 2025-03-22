5e7188beadac60eadced2715e410df00ea6a6bc0
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-03-22 18:20:21 +0100

    ok

--- routing/src/app/app.routes.ts
+++ routing/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AbouteComponent } from './aboute/aboute.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'home', component:HomeComponent },
+    { path: 'aboute', component: AbouteComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', component: PageNotFoundComponent },
+
+];
