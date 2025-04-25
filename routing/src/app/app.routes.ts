de0a6c1b604559db49f5752a5223766c1a81daec
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-04-25 20:03:54 +0200

    ok

--- routing/src/app/app.routes.ts
+++ routing/src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { AbouteComponent } from './aboute/aboute.component';
 import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CoursesComponent } from './courses/courses.component';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'aboute' },
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', component:HomeComponent },
-    { path: 'home/:id/:name', component:HomeComponent },
     { path: 'aboute', component: AbouteComponent },
     { path: 'contact', component: ContactComponent },
+    { path: 'courses', component: CoursesComponent },
     { path: '**', component: PageNotFoundComponent },
 
 ];
